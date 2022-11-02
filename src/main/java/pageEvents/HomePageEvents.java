02ea473e84f59ad675537856e5e85defdbdb9d16
nomesh_inivos <nomesh.desilva@inivossl.com>
2022-11-02 23:11:29 +0530

    added amazon website test basic test cases

--- src/main/java/pageEvents/HomePageEvents.java
+++ src/main/java/pageEvents/HomePageEvents.java
@@ -1,12 +1,38 @@
 package pageEvents;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.HomePageElements;
+import utils.BaseTest;
 import utils.ElementFetch;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
 
 public class HomePageEvents {
 
     public void clickOnSignInButton(){
         ElementFetch elementFetch = new ElementFetch();
+
+        //Maximize window
+        BaseTest.getDriver().manage().window().maximize();
+
+        //Set the Script Timeout to 20 seconds
+        BaseTest.getDriver().manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+
+        //Declare and set the start time
+        long start_time = System.currentTimeMillis();
+
+        JavascriptExecutor js = (JavascriptExecutor) BaseTest.getDriver();
+        //Call executeAsyncScript() method to wait for 5 seconds
+        js.executeAsyncScript("window.setTimeout(arguments[arguments.length - 1], 5000);");
+
+        WebDriverWait wait = new WebDriverWait(BaseTest.getDriver(),Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(HomePageElements.SKIP_COOKIES_NOTICE_POPUP)));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(HomePageElements.SKIP_COOKIES_NOTICE_BUTTON)));
         elementFetch.getWebElement("XPATH", HomePageElements.signInButton).click();
     }
 }
--- src/main/java/pageObjects/HomePageElements.java
+++ src/main/java/pageObjects/HomePageElements.java
@@ -4,6 +4,8 @@ package pageObjects;
 public interface HomePageElements {
 
     String signInButton = "//span[text()='Sign in']";
+    String SKIP_COOKIES_NOTICE_BUTTON = "//*[@id=\"nav-main\"]/div[1]/div/div/div[3]/span[1]/span/input";
+    String SKIP_COOKIES_NOTICE_POPUP = "//*[@id=\"nav-main\"]/div[1]/div/div/div[3]/span[1]/span/span";
 
 
 }
